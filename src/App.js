187ff1e3352958efb93c249fa3a0c356ca9b8e01
chunsen.zhao <chunsen.zhao@accenture.com>
2020-02-16 20:52:50 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  onSubmit(e) {
+    e.persist();
+    console.log(e.target.name.value,e.target.pwd.value);
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <form className="warp" onSubmit={this.onSubmit}>
+        <label>
+          name:<input name="name" />
+        </label>
+        <label>
+          pwd:<input name="pwd" />
+        </label>
+        <button type="submit" value="Submit">
+          submit
+        </button>
+      </form>
+    );
+  }
 }
 
 export default App;
