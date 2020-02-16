4a23c01fe7bbd7fe1265e5fc87772ae6a83d43ad
chunsen.zhao <chunsen.zhao@accenture.com>
2020-02-16 20:46:18 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+function Numbers(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="number_warp">
+      {props.nums.map(e =>
+        <button key={e.label}>
+          {e.label}
+        </button>
+      )}
     </div>
   );
+}
+const buttons = [
+  {
+    label: "0"
+  },
+  {
+    label: "1"
+  },
+  {
+    label: "2"
+  },
+  {
+    label: "3"
+  },
+  {
+    label: "4"
+  },
+  {
+    label: "5"
+  },
+  {
+    label: "6"
+  },
+  {
+    label: "7"
+  },
+  {
+    label: "8"
+  },
+  {
+    label: "9"
+  }
+];
+function Actions(props) {
+  return (
+    <div className="action_warp">
+      {props.funs.map(e =>
+        <button key={e.label}>
+          {e.label}
+        </button>
+      )}
+    </div>
+  );
+}
+const funs = [
+  {
+    label: "+"
+  },
+  {
+    label: "-"
+  },
+  {
+    label: "*"
+  },
+  {
+    label: "/"
+  },
+  {
+    label: "C"
+  },
+  {
+    label: "="
+  }
+];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="warp">
+        <input />
+        <div className="result" />
+        <Actions funs={funs} />
+        <Numbers nums={buttons} />
+      </div>
+    );
+  }
 }
 
 export default App;
