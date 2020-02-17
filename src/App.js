c10370b5b6441c24694afadd4a478193156f9779
chunsen.zhao <chunsen.zhao@accenture.com>
2020-02-17 14:09:50 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+import React from "react";
+import "./App.css";
+function PWDstrength(str) {
+  if (str.length > 8) {
+    return "Strong";
+  }
+  if (str.length > 4) {
+    return "medium";
+  }
+  if (str.length <= 4) {
+    return "weak";
+  }
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      pwd: ""
+    };
+  }
+  onSubmit(e) {
+    e.persist();
+    console.log(this.state);
+  }
+  onChange = e => {
+    e.persist();
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  render() {
+    return (
+      <div className="warp">
+        <label>
+          name:<input
+            name="name"
+            value={this.state.name}
+            onChange={this.onChange}
+          />
+        </label>
+        <label>
+          pwd:<input
+            name="pwd"
+            value={this.state.pwd}
+            onChange={this.onChange}
+          />
+        </label>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.pwd && `密码强度:${PWDstrength(this.state.pwd)}`}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <button onClick={this.onSubmit}>submit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
